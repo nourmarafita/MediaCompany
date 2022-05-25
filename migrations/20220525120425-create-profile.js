7570e356e88429f5485a9379463fcaa59d6e8611
Ade Farhan <adefarhanzikri@gmail.com>
2022-05-25 21:00:39 +0700

    commit 1

---
+++ migrations/20220525120425-create-profile.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      photo: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      jabatan: {
+        type: Sequelize.STRING
+      },
+      numberPhone: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Profiles');
+  }
+};
